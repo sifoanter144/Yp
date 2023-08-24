49dd6a48f30b242d0053af01355498451cc3e652
ZAIN-78 <142612990+ZAIN-78@users.noreply.github.com>
2023-08-24 23:09:34 +0500

    Update ZAIN.py

--- ZAIN.py
+++ ZAIN.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('git pull')
 ZAIN=platform.architecture()[0]
-if VENOM=="32bit":exit(' Your Device Not Support This tool')
-elif VENOM=="64bit":__import__("BS")
+if ZAIN=="32bit":exit(' Your Device Not Support This tool')
+elif ZAIN=="64bit":__import__("BS")
  
